8ad6efa8a28e9f8b254f278ac30231d4e092c05f
boyko13 <81235503+boyko13@users.noreply.github.com>
2024-04-03 23:25:13 +0200

    Update and rename PhoneBook.cs to Phone_Book/PhoneBook.cs

---
+++ Phone_Book/PhoneBook.cs
@@ -0,0 +1,48 @@
+﻿namespace PhoneBook;
+
+public class PhoneBook
+{
+    public List<Contact> Contacts { get; set; } = new List<Contact>();
+
+    public void AddContacts(Contact contact)
+    {
+        Contacts.Add(contact);
+    }
+
+    private void ContactDetails(Contact contact)
+    {
+        Console.WriteLine($"Contact: {contact.Name}, {contact.Number}");
+    }
+
+    public void DisplayContact(string number)
+    {
+        var contact = Contacts.FirstOrDefault(c => c.Number == number);
+
+        if (contact == null)
+        {
+            Console.WriteLine("Contact not found");
+        }
+        else
+        {
+            ContactDetails(contact);
+
+        }
+    }
+
+    public void DisplayAllContacts()
+    {
+        foreach (var contact in Contacts)
+        {
+            ContactDetails(contact);
+        }
+    }
+
+    public void MatchingContacts(string searchPhrase)
+    {
+        var matchingContacts = Contacts.Where(c => c.Name.Contains(searchPhrase)).ToList();
+        foreach (var contact in matchingContacts)
+        {
+            ContactDetails(contact);
+        }
+    }
+}
