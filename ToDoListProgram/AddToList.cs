623806d55fdb8718ceb6423dec70043132635164
boyko13 <81235503+boyko13@users.noreply.github.com>
2024-04-03 23:07:26 +0200

    Create AddToList.cs

---
+++ ToDoListProgram/AddToList.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    internal class AddToList
+    {
+        List<string> Todolist = new List<string>();
+        public string ToDo { get; set; }
+            
+    }
+}
